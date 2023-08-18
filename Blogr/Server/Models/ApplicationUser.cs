30236a4f5b961a7786333689664898b6f6a11d76
scottmumby <48021218+scoomthesnooper@users.noreply.github.com>
2023-08-18 02:38:30 +0100

    Added Blog and Application User Models

--- Blogr/Server/Models/ApplicationUser.cs
+++ Blogr/Server/Models/ApplicationUser.cs
@@ -1,8 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Blogr.Data;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blogr.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        //User Creation Date
+        private DateTime u_CreationDate { get; set; }
+
+        private string u_FirstName { get; set; } = null!;
+
+        private string u_LastName { get; set; } = null!;
+        //A string location of where a users photo is stored within the servers file system
+        private string u_Photo { get; set; } = null!;
+
+        private ICollection<Blog> u_Blogs { get; set; } = null!;
     }
 }
---
+++ Blogr/Server/Models/Blog.cs
@@ -0,0 +1,23 @@
+﻿using Blogr.Server.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blogr.Data
+{
+    public class Blog
+    {
+        [Key]
+        private int b_ID { get; set; }
+
+        private string b_Title { get; set; } = null!;
+
+        [Key]
+        private ApplicationUser b_user { get; set; } = null!;
+
+        private DateTime b_CreationDate { get; set; }
+
+        private DateTime b_UpdatedDate { get; set; }
+
+        private string b_Content { get; set; } = null!;
+
+    }
+}
