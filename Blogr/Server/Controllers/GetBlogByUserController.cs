bc7ea786ca7b9b9e58151b876317e198d6576b5d
scottmumby <48021218+scoomthesnooper@users.noreply.github.com>
2023-08-22 03:32:48 +0100

    Added get current users blogs
    Implemented ability for user to view their own blogs

---
+++ Blogr/Server/Controllers/GetBlogByUserController.cs
@@ -0,0 +1,66 @@
+﻿using Blogr.Data;
+using Blogr.Server.Data;
+using Blogr.Server.Models;
+using Blogr.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Blogr.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    public class GetBlogByUserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public GetBlogByUserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        [Route("api/GetBlogByCurrentUser")]
+        [HttpGet]
+        public async Task<ActionResult<List<BlogDisplay>>> GetUserBlogs()
+        {
+            List<BlogDisplay> blogList = new List<BlogDisplay>();
+            var user = await _userManager.FindByIdAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var userBlogs = _context.Blogs.Where(u => u.b_User == user);
+            foreach(var blog in userBlogs)
+            {
+                BlogDisplay blogDisplay = new BlogDisplay();
+                blogDisplay.Title = blog.b_Title;
+                blogDisplay.CreatorImgPath = user.u_Photo.path;
+                blogDisplay.CreatorFirstName = user.u_FirstName;
+                blogDisplay.CreatorLastName = user.u_LastName;
+                blogDisplay.CreationDate = blog.b_CreationDate;
+                blogDisplay.UpdatedDate = blog.b_CreationDate;
+                blogDisplay.Content = blog.b_Content;
+                blogList.Add(blogDisplay);
+            }
+            return Ok(blogList);
+        }
+        [Route("api/GetBlogByUser")]
+        [HttpGet]
+        public async Task<ActionResult<List<BlogDisplay>>> GetUserBlogs(string userId)
+        {
+            List<BlogDisplay> blogList = new List<BlogDisplay>();
+            var user = await _userManager.FindByIdAsync(userId);
+            foreach (var blog in user.u_Blogs)
+            {
+                BlogDisplay blogDisplay = new BlogDisplay();
+                blogDisplay.Title = blog.b_Title;
+                blogDisplay.CreatorImgPath = user.u_Photo.path;
+                blogDisplay.CreatorFirstName = user.u_FirstName;
+                blogDisplay.CreatorLastName = user.u_LastName;
+                blogDisplay.CreationDate = blog.b_CreationDate;
+                blogDisplay.UpdatedDate = blog.b_CreationDate;
+                blogDisplay.Content = blog.b_Content;
+                blogList.Add(blogDisplay);
+            }
+            return Ok(blogList);
+        }
+    }
+}
---
+++ Blogr/Shared/BlogDisplay.cs
@@ -0,0 +1,27 @@
+﻿using Blogr.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogr.Shared
+{
+    public class BlogDisplay
+    {        
+        public string Title { get; set; } = null!;
+
+        public string CreatorImgPath { get; set; }
+
+        public string CreatorFirstName { get; set; } = null!;
+
+        public string CreatorLastName { get; set; } = null!;
+
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public BlogContent Content { get; set; } = null!;
+    }
+}
