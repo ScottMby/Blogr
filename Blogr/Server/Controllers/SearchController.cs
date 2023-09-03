a1df30c32c8f8ba9a8b83ed1f70419929b2cfdb0
scottmumby <48021218+scoomthesnooper@users.noreply.github.com>
2023-09-03 20:08:41 +0100

    Add// Search Bar

---
+++ Blogr/Server/Controllers/SearchController.cs
@@ -0,0 +1,44 @@
+﻿using Blogr.Data;
+using Blogr.Server.Data;
+using Blogr.Server.Models;
+using Blogr.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogr.Server.Controllers
+{
+    [Route("api/Search")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public SearchController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Route("{searchQuery}")]
+        [HttpGet]
+        public async Task<ActionResult<BlogDisplay>> Get([FromRoute(Name = "searchQuery")] string searchQuery)
+        {
+            try
+            {
+                List<Blog?> blogs = _context.Blogs
+                    .Where(u => u.Title.Contains(searchQuery))
+                    .Include("Content")
+                    .Include("User")
+                    .ToList();
+
+                return Ok(GetBlog.Get(blogs, null, _userManager).Result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
